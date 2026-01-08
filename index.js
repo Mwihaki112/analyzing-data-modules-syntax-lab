734ae0622cb0bbea8331cca9bf1b2e5e42d3df36
Agnes Mwihaki <moringa@DESKTOP-89EPAHO.localdomain>
2026-01-08 12:29:52 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+require("datejs");
 
+function combineUsers(...args){
+  const combinedObject = {
+    users:[]
+  };
+
+  for(let arr of args){
+    combinedObject.users.push(...arr);
+  }
+  let today = Date.today();
+  combinedObject.merge_date = today.toString("M/d/yyyy");
+
+  return combinedObject;
+}
+
+const result = combineUsers(
+  ["Bojack", "Horseman"], 
+  ["Todd", "Chavez"], 
+  ["Vanessa", "Geko"],
+);
+
+console.log(result);
 
 
 module.exports = {
